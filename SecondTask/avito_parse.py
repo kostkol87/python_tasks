49d0717aa0476ab2fad3b10da4f21b4119497b5c
kostkol87 <kostkol87@gmail.com>
2015-03-01 20:19:56 +0200

    simple refactor

--- SecondTask/avito_parse.py
+++ SecondTask/avito_parse.py
@@ -1,79 +1,52 @@
 # -*- coding: utf-8 -*-
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
-import json
 import sys
 
-class AvitoParse:
-    # url = sys.argv[1]
-    url = 'https://www.avito.ru/sankt-peterburg/kvartiry/2-k_kvartira_53_m_15_et._443405288'
-    html_doc = urlopen(url).read()
-    soup = BeautifulSoup(html_doc)
+
+# url = sys.argv[1]
+url = 'https://www.avito.ru/sankt-peterburg/kvartiry/2-k_kvartira_53_m_15_et._443405288'
+html_doc = urlopen(url).read()
+soup = BeautifulSoup(html_doc)
 
 
-    # id
-    id = url[-9:]
-    print id
+# id
+id = url[-9:]
+print id
 
-    # title
-    titleTag = soup.html.head.title
-    title = titleTag.string
-    print title
+# title
+titleTag = soup.html.head.title
+title = titleTag.string
+print title
 
-    #city
-    city = (soup.find('div', id='map')).string
-    print city
+#city
+city = (soup.find('div', id='map')).string
+print city
 
-    # address
-    met=''
-    addr = soup.find('span', itemprop="streetAddress")
-    print addr.string
-    metro = str(addr.find_parent())
-    i=6
-    while(metro[i] !=','):
-        met += metro[i]
-        i += 1
-    print met
+# address
+met=''
+addr = soup.find('span', itemprop="streetAddress")
+print addr.string
+metro = str(addr.find_parent())
+i=6
+while(metro[i] !=','):
+    met += metro[i]
+    i += 1
+print met
 
-    #content
-    description=''
-    cont = soup.find('div', id='desc_text')
-    for descr in cont:
-        description += descr.string
-    print description
+#content
+description=''
+cont = soup.find('div', id='desc_text')
+for descr in cont:
+    description += descr.string
+print description
 
-    # price
-    scripts = str(soup.find_all('script'))
-    price_inx = str(scripts).find('par_price')
-    price = ''
-    while (scripts[price_inx+13] != '"'):
-        price += scripts[price_inx+13]
-        price_inx += 1
-    print price
-
-    params = {'id': id, "title" : title, 'city' : city, 'address':{'street': addr, 'metro' : met}, 'desctiption': description, 'price': price}
-
-parser = AvitoParse
-output=open("jsonout.txt", "w")
-json.dump(parser.params, output)
-output.close()
-# # ================================================
-
-#
-# #
-# class pObj(object):
-#     pass
-#
-# myHead=pObj()
-# myHead.hair="Black"
-# myHead.eyes={}
-# myHead.eyes['left']="Brown"
-# myHead.eyes['right']="Brown"
-#
-# output=open("jsonout.txt", "w") # откроем файл для записи JSON-объекта
-#
-# #json.dump(myHead, output) # Не сработвет
-# json.dump(myHead.eyes, output)
-#
-# output.close()
+# price
+scripts = str(soup.find_all('script'))
+price_inx = str(scripts).find('par_price')
+price = ''
+while (scripts[price_inx+13] != '"'):
+    price += scripts[price_inx+13]
+    price_inx += 1
+print price
 
